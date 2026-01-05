9252d53bee31a9939eb6eb7ef82b3177d5a39ea5
wjz <wjz2930608106@qq.com>
2026-01-05 18:02:25 +0800

    test2

--- assets/js/members-data.js
+++ assets/js/members-data.js
@@ -28,6 +28,17 @@ const labMembers = {
             google_scholar: "https://scholar.google.com/citations?user=EXAMPLE",
             email: "kenli@example.com",
             github: "https://github.com/kenli"
+        },
+{
+            name: "Ken Li",
+            title: "Ph.D. Student",
+            period: "2026.09~Present",
+            co_supervised: "Co. w. Prof. Supervisor Name",
+            photo: "./assets/img/people/liken.jpg",
+            homepage: "https://kenli.example.com",
+            google_scholar: "https://scholar.google.com/citations?user=EXAMPLE",
+            email: "kenli@example.com",
+            github: "https://github.com/kenli"
         }
     ],
     
---
+++ people/likenno2.md
@@ -0,0 +1,13 @@
+---
+name: Ken Li
+title: Ph.D. Student
+period: 2026.09~Present
+photo: ./assets/img/people/liken.jpg
+homepage: https://kenli.example.com
+google_scholar: https://scholar.google.com/citations?user=EXAMPLE
+email: kenli@example.com
+github: https://github.com/kenli
+co_supervised: Co. w. Prof. Supervisor Name
+order: 2
+---
+
